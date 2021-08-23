7247e54ff5580b55ed52ea78f7ed5422a3727505
pratyaydeep <pratyaydeepghanta@gmail.com>
2021-08-24 02:54:09 +0530

    Completed CheckList Model functionalities, its views and serializers

--- checklist/core/urls.py
+++ checklist/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from core.views import TestAPIView, CheckListAPIView
+from core.views import (CheckListsAPIView,
+                        CheckListAPIView)
 
 urlpatterns = [
-    path('', TestAPIView.as_view()),
-    path('api/checklists/', CheckListAPIView.as_view()),
+    path('api/checklists/', CheckListsAPIView.as_view()),
+    path('api/checklist/<int:pk>/', CheckListAPIView.as_view()),
 ]
